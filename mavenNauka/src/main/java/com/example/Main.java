266646c124088f36fc2f1fa10b57df7a1ba3a442
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-10-09 13:33:31 +0200

    MavenNauka

---
+++ mavenNauka/src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+// configuration command line:
+// compile exec:java -Dexec.mainClass=com.example.Main
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
