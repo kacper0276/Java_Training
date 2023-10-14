b23627c33b42f02a6ea615b8a96f5b146fea1b35
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-10-14 13:58:08 +0200

    Hibernate Nauka

---
+++ HibernateNauka/src/main/java/org/example/dao/DAOUsageExample.java
@@ -0,0 +1,30 @@
+package org.example.dao;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+public class DAOUsageExample {
+    public static void main(String[] args) {
+
+        ProjectService projectService = new ProjectService(new ProjectDAODB());
+
+        int randId = (new Random()).nextInt(99);
+        Project project = new Project("Project #" + randId, new Date());
+        projectService.persist(project);
+
+        List<Project> projects = projectService.findAll();
+        projects.stream().forEach(p -> System.out.println(p));
+
+        Project projectFromDB = projectService.findById(projects.get(0).getId());
+        System.out.println("\nProject from DB: " + projectFromDB);
+
+        projectService.delete( projectFromDB );
+        System.out.println("Project id: " + projectFromDB.getId() + " deleted");
+
+        System.out.println("\nProjects list after delete:");
+        projects = projectService.findAll();
+        projects.stream().forEach(p -> System.out.println(p));
+
+    }
+}
